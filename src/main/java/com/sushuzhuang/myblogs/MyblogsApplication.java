a8cd8b6937ee09fed6ade7fba13cff230b5094f7
1434545037@qq.com <1434545037@qq.com>
2018-08-20 15:11:55 +0800

    dabao

--- src/main/java/com/sushuzhuang/myblogs/MyblogsApplication.java
+++ src/main/java/com/sushuzhuang/myblogs/MyblogsApplication.java
@@ -1,8 +1,16 @@
 package com.sushuzhuang.myblogs;
 
+
+
 import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
 
 @SpringBootApplication
 @MapperScan("com.sushuzhuang.myblogs.mapper")
@@ -11,4 +19,6 @@ public class MyblogsApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyblogsApplication.class, args);
     }
+
+
 }
---
+++ src/main/java/com/sushuzhuang/myblogs/ServletInitializer.java
@@ -0,0 +1,15 @@
+package com.sushuzhuang.myblogs;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyblogsApplication.class);
+    }
+}
